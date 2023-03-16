0c18bbca3ad724c418524db39d55e8412b01659e
eli musa <eli.musa.173@gmail.com>
2023-03-17 00:56:12 +0300

    changesv2.0

--- src/styles/global.js
+++ src/styles/global.js
@@ -2,6 +2,8 @@ import { createGlobalStyle } from "styled-components"
 
 export const GlobalStyle=createGlobalStyle`{
     body{
+        justify-content: center;
+        align-items: center;
         margin:0px;
         padding:0px;
         font-family: Arial, san-serif;
