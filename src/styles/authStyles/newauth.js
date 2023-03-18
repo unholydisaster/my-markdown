1a3fccd0c7efdc86458192f48fe9c2c576c9f947
eli musa <eli.musa.173@gmail.com>
2023-03-18 11:40:39 +0300

    changesv3.1

--- src/styles/authStyles/newauth.js
+++ src/styles/authStyles/newauth.js
@@ -1,9 +1,12 @@
 import styled from 'styled-components';
 
 export const FormContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
+position: absolute;
+top: 70px;
+display: flex;
+justify-content: center;
+align-items: center;
+height: 100vh;
 `;
 
 export const FormWrapper = styled.div`
